634d40ec512eff8e563daf9b1c6afb405af6f70b
OsamaMohammad61 <osamamohammad343@gmail.com>
2024-06-12 12:44:28 +0300

    Solved the connection error

--- controllers/Courses.js
+++ controllers/Courses.js
@@ -3,16 +3,15 @@ const Course = require('../models/Course')
 const addCourse = async (req, res) => {
   //let { name, Description, Instructors } = req.body
   console.log(req.body)
-  /*
+
   try {
-    const course = await new Course(req.body)
-    console.log(course.name)
+    const course = new Course.create(req.body)
   } catch (err) {
     console.error(
       'this is the error in the add course function in the Courses.JS controller file  ' +
         err
     )
-  }*/
+  }
 }
 
 module.exports = { addCourse }
--- server.js
+++ server.js
@@ -2,26 +2,22 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 const socketIO = require('socket.io')
-const { default: mongoose } = require('mongoose')
 const http = require('http')
 const { register, login } = require('./controllers/User') // Destructure the imports
 const chatRoutes = require('./routes/Chat')
-<<<<<<< HEAD
+const courseRoutes = require('./routes/courses')
+
+const db = require('./db')
+
 // mongoose.connect('', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // });
 const app = express()
 const server = http.createServer(app)
-=======
-
-mongoose.connect('', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
 
 // const serverI = http.createServer(server)
->>>>>>> 9516cc7 (Almost Done with the add course function)
+
 const io = socketIO(server)
 const PORT = process.env.PORT || 3001
 app.use(cors())
@@ -31,6 +27,7 @@ app.use(express.urlencoded({ extended: false }))
 app.post('/register', register)
 app.post('/login', login)
 app.use('/chats', chatRoutes)
+app.use('/course', courseRoutes)
 app.use('/', (req, res) => {
   res.send('Connected!')
 })
