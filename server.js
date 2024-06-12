c8061a9aff0b05b47820ce3f3f28cc9d27eb2254
Ghazialsardeeh <ghazialbakar@gmail.com>
2024-06-12 01:01:21 -0700

    chaaat

--- server.js
+++ server.js
@@ -1,37 +1,29 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-const socketIO = require('socket.io')
-const { default: mongoose } = require('mongoose')
-const { server } = require('websocket')
-const register = require('./controllers/User')
-const Login = require('./controllers/User')
-const chatRoutes = require('./routes/Chat')
-
-mongoose.connect('', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const app = express()
-// const serverI = http.createServer(server)
-const io = socketIO(server)
-
-const PORT = process.env.PORT || 3001
-
-app.use(cors())
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-
-app.post('/register', register)
-app.post('/login', Login)
-app.use('/chats', chatRoutes)
-
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const socketIO = require('socket.io');
+const { default: mongoose } = require('mongoose');
+const http = require('http');
+const { register, login } = require('./controllers/User'); // Destructure the imports
+const chatRoutes = require('./routes/Chat');
+// mongoose.connect('', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.post('/register', register); 
+app.post('/login', login); 
+app.use('/chats', chatRoutes);
 app.use('/', (req, res) => {
-  res.send(`Connected!`)
-})
-
-app.listen(PORT, () => {
-  console.log(`Running Express server on Port ${PORT} . . .`)
-})
+  res.send('Connected!');
+});
+server.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`);
+});
