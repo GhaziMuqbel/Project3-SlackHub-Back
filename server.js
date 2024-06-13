8e00c99c336f6609a70bc398986b427cf6770e3e
User <osamamohammad343@gmail.com>
2024-06-13 20:26:21 +0300

    resolved the conflict

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ const { Server } = require('socket.io')
 
 const PORT = process.env.PORT || 3001
 const corsOptions = {
-  origin: 'http://localhost:5501', // Update this to your client's origin
+  origin: ['http://localhost:5501', 'http://127.0.0.1:5501'], // Allow both origins
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true
-}
+};
 app.use(cors(corsOptions))
 app.use(logger('dev'))
 app.use(express.json())
@@ -39,11 +39,11 @@ const server = http.createServer(app)
 // Create a Socket.IO server
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5501', // Update this to your client's origin
+    origin: ['http://localhost:5501', 'http://127.0.0.1:5501'], // Allow both origins
     methods: ['GET', 'POST'],
     credentials: true
   }
-})
+});
 // Users dictionary to keep track of connected users
 const users = {}
 io.on('connection', (socket) => {
